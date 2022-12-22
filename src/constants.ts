543de9b6fa7954ebac3a35f7b3a228cbed53046c
CryptoSnowPrince <topdirector2017@gmail.com>
2022-12-22 22:59:28 +0800

    update

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ export const CONTRACT_NAME = 'cis2_wCCD';
  * Should match the subindexes of the instances targeted.
  * V1 Module reference on testnet: cc285180b45d7695db75c29dee004d2e81a1383880c9b122399bea809196c98f
  */
-export const WCCD_CONTRACT_INDEX = 2059n;
+export const WCCD_CONTRACT_INDEX = 2308n;
 
 export const CONTRACT_SUB_INDEX = 0n;
 
